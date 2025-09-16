b8c05a32e4e3480362c305e2a4d6085c354e9239
dingyisun0101 <dingyisun0122@gmail.com>
2025-09-16 01:50:46 +0000

    Added licensing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-#[allow(unused_imports)]
-use physics_in_parallel::math_fundations::{tensor::dense::Tensor as TensorDense, tensor::sparse::Tensor as TensorSparse};
-use physics_in_parallel::math_fundations::{vector_list::VectorList};
+// #[allow(unused_imports)] use physics_in_parallel::math_fundations::{tensor::dense::Tensor as TensorDense, tensor::sparse::Tensor as TensorSparse};
+// #[allow(unused_imports)] use physics_in_parallel::math_fundations::{vector_list::VectorList};
 
 
 fn main() {
